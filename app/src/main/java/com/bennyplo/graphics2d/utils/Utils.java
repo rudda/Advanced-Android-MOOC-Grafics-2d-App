29e68cb4289f1d72407c9cfd41ee6af170eefc0e
Ruddá Beltrão <beltrao.rudah@gmail.com>
2020-02-18 19:58:38 -0400

    chore: get height and width from getDisplayMetrics from Utils class

--- app/src/main/java/com/bennyplo/graphics2d/utils/Utils.java
+++ app/src/main/java/com/bennyplo/graphics2d/utils/Utils.java
@@ -1,4 +1,22 @@
 package com.bennyplo.graphics2d.utils;
 
+import android.content.Context;
+
+/**
+ * class create by Rudda and aims to provides some screen utils resources
+ * in order to reuse into all project tasks.
+ * :)
+ * I'm lucky guy
+ */
 public class Utils {
+
+    public static int getScreenWidth(Context context){
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    public static int getScreenHeight(Context context){
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+
 }
